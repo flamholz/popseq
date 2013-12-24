04abad2adb5e5fdb1419cff963343085dec1f275
Avi Flamholz <flamholz@gmail.com>
2013-12-23 16:59:57 -0800

    Lots of cleanup in the read filtering script.

--- scripts/filter_reads.py
+++ scripts/filter_reads.py
@@ -26,160 +26,89 @@ import time
 from os import path
 from Bio import SearchIO
 from Bio import SeqIO
+from scripts.util import filename_util
+from scripts.util import command_util
 
 
-def MakeFASTAFilename(fastq_path, dest_dir):
-	"""Makes a FASTA filename for the given FASTQ file.
-
-	Args:
-		fastq_path: path to the FASTQ file.
-		dest_dir: where you want to save the FASTA file.
+def ConvertFASTQToFASTA(fastq_fnames, output_dir):
+	"""Calls out to compiled executable to convert FASTQ files to FASTA.
 	
-	Returns:
-		The FASTA filename.
-	"""
-	head, tail = path.split(fastq_path)
-	name, ext = path.splitext(tail)
-	return path.join(dest_dir, '%s.fa' % name)
-
-
-def MakeFilteredFASTAFilename(fasta_path):
-	"""Makes a filename for a filtered version of the given file.
-
 	Args:
-		fasta_path: path to the FASTA file.
-	
+		fastq_fnames: iterable of paths to FASTQ files.
+
 	Returns:
-		The FASTA filename.
+		A list of paths to the FASTA files.
 	"""
-	name, ext = path.splitext(fasta_path)
-	return '%s_filtered.fa' % name
-
-
-def MakePSLFilename(fasta_path, dest_dir):
-	"""Makes a BLAT PSL filename for the given FASTA file.
-
-	Args:
-		fasta_path: path to the FASTA file.
-		dest_dir: where you want to save the PSL.
-	
-	Returns:
-		The PSL filename.
-	"""
-	head, tail = path.split(fasta_path)
-	name, ext = path.splitext(tail)
-	return path.join(dest_dir, '%s_aligned.psl' % name)
-
-
-def ToFASTACommand(fastq_path, fasta_path):
-	"""Returns a shell command to convert the given FASTQ to FASTA."""
-	return ['fastq_to_fasta',
-		    '-i', fastq_path,
-		    '-o', fasta_path,
-			'-Q33']
-
-
-def BLATCommand(db_fname, query_fname, output_fname,
-				**kwargs):
-	"""Returns a shell to BLAT search with."""
-	command = ['blat', db_fname, query_fname, output_fname,
-			   '-q=dna', '-t=dna']  # DB and Query are DNA.
-	if 'blat_tile_size' in kwargs:
-		command.append('-tileSize=%d' % kwargs['blat_tile_size'])
-	if 'blat_step_size' in kwargs:
-		command.append('-stepSize=%d' % kwargs['blat_step_size'])
-	if 'blat_min_score' in kwargs:
-		command.append('-minScore=%d' % kwargs['blat_min_score'])
-	return command
-
-
-def CheckInstalled(name):
-	"""Assertion fails if the given shell command is not available."""
-	test_command = ['which', name]
-	ret = subprocess.check_output(test_command)
-	assert ret, '%s is not installed!' % name
-
-
-def CheckAllExist(paths):
-	"""Assertion fails if any of the given paths does not exist."""
-	for fname in paths:
-		assert path.exists(fname), '%s does not exist!' % fname
-
-
-def Main():
-	parser = argparse.ArgumentParser(description='Filter reads.')
-	parser.add_argument("database_filename",
-      					help="Path to FASTA file containing DB to align reads to.")
-	parser.add_argument("-r", "--read_filenames", nargs='+', help="Path to FASTQ file containing reads.")
-	parser.add_argument("-t", "--tmp_dir",
-						default="_read_filter_data",
-					    help="Path to use to store intermediate files.")
-	parser.add_argument("--blat_tile_size", type=int, default=10,
-						help="Tile size to use for BLAT search.")
-	parser.add_argument("--blat_step_size", type=int, default=3,
-						help="Step size to use for BLAT search.")
-	parser.add_argument("--blat_min_score", type=int, default=10,
-						help="Minimum score to retain a BLAT match.")
-	args = parser.parse_args()
-
-	# Check that everything we need exists.
-	map(CheckInstalled, ['fastq_to_fasta', 'blat'])
-
-	read_filenames = map(glob.glob, args.read_filenames)
-	read_filenames = list(itertools.chain(*read_filenames))
-	read_filenames = filter(lambda n: n.endswith('fq') or n.endswith('fastq'),
-							read_filenames)
-	print 'Read filenames:', read_filenames
-	assert len(read_filenames) > 0, 'Must provide reads!'
-	CheckAllExist(read_filenames)
-
-	# Make the temporary directory if needed.
-	if not path.exists(args.tmp_dir):
-		os.makedirs(args.tmp_dir)
-
-	print 'Converting FASTQ to FASTA'
-	start_ts = time.time()
-
-	fasta_fnames = []
-	for fname in read_filenames:
-		out_fname = MakeFASTAFilename(fname, args.tmp_dir)
+	fasta_fnames= []
+	for fname in fastq_fnames:
+		out_fname = filename_util.MakeFASTAFilename(fname, output_dir)
 		fasta_fnames.append(out_fname)
 		if path.exists(out_fname):
 			print '\tSkipping conversion of %s as output exists' % fname
 			continue
 
-		to_fasta_command = ToFASTACommand(fname, out_fname)
+		print 'Writing FASTA output to', out_fname
+		to_fasta_command = command_util.ToFASTACommand(fname, out_fname)
 		ret = subprocess.call(to_fasta_command)
 		assert ret == 0, '%s failed' % to_fasta_command[0]
+	return fasta_fnames
+
+
+def AlignReadsToDB(insert_db_fname, fasta_fnames, output_dir,
+				   output_filename_postfix=None,
+				   blat_tile_size=10,
+				   blat_step_size=3,
+				   blat_min_score=10):
+	"""Calls out to BLAT to align reads to the given sequences.
+
+	Args:
+		insert_db_fname: the path to the file containing the sequences to align to.
+		fasta_fnames: the read filenames to align.
 	
-	duration = time.time() - start_ts
-	print 'Finished converting to FASTA, took %.3f seconds' % duration
-
-	print 'Aligning reads to database at %s' % args.database_filename
-	start_align_ts = time.time()
-	psl_fnames = []
-	blat_kwargs = {'blat_step_size': args.blat_step_size,
-				   'blat_tile_size': args.blat_tile_size,
-				   'blat_min_score': args.blat_min_score}
-
+	Returns:
+		A list of PSL filenames with the alignment output.
+	"""
+	insert_psl_fnames = []
 	for fasta_fname in fasta_fnames:
-		psl_fname = MakePSLFilename(fasta_fname, args.tmp_dir)
-		psl_fnames.append(psl_fname)
+		psl_fname = filename_util.MakePSLFilename(
+			fasta_fname, output_dir, postfix=output_filename_postfix)
+		insert_psl_fnames.append(psl_fname)
 		if path.exists(psl_fname):
 			print '\tSkipping alignment of %s as output exists' % fasta_fname
 			continue
 
-		blat_command = BLATCommand(
-			args.database_filename, fasta_fname, psl_fname,
-			**blat_kwargs)
+		print '\tWriting output to', psl_fname
+		blat_command = command_util.BLATCommand(
+			insert_db_fname, fasta_fname, psl_fname,
+			blat_tile_size=blat_tile_size,
+			blat_step_size=blat_step_size,
+			blat_min_score=blat_min_score)
 		ret = subprocess.call(blat_command)
 		assert ret == 0, 'blat failed for %s!' % fasta_fname
-	align_duration = time.time() - start_align_ts
-	print 'Finished BLAT alignment to database, took %.3f seconds' % duration
+	return insert_psl_fnames
 
-	start_filter_ts = time.time()
+
+def FilterFASTAByPSL(fasta_fnames, psl_fnames, output_dir,
+					 output_filename_postfix=None):
+	"""Produces a FASTA file with only those sequences in the PSL.
+
+	Iterates over pairs of FASTA and PSL files and produces a new
+	FASTA file containing only those sequences for which there was a 
+	record in the PSL file.
+
+	Args:
+		fasta_fnames: the FASTA filenames.
+		psl_fnames: the PSL (BLAT output) filenames in the same order.
+	
+	Returns:
+		A list of paths to filtered FASTA files.
+	"""
+	filtered_fnames = []
 	for fasta_fname, psl_fname in zip(fasta_fnames, psl_fnames):
-		filtered_fname = MakeFilteredFASTAFilename(fasta_fname)
+		filtered_fname = filename_util.MakeFASTAFilename(
+			fasta_fname, dest_dir=output_dir,
+			postfix=output_filename_postfix)
+		filtered_fnames.append(filtered_fname)
 		if path.exists(filtered_fname):
 			print 'Skipping filtering of %s as output exists' % psl_fname
 
@@ -206,12 +135,89 @@ def Main():
 		print '\tWriting output to', filtered_fname
 		SeqIO.write(retained, filtered_fname, 'fasta')
 
-		# Force the GCs hand since these may be very big.
+		# Force delete these lists since they might be very big.
 		del retained
 		del ids_with_hits
+	return filtered_fnames
 
-	duration = time.time() - start_filter_ts
-	print 'Finished filtering output, took %.3f seconds' % duration
+
+def Main():
+	parser = argparse.ArgumentParser(description='Filter reads.')
+	parser.add_argument("-i", "--insert_db_filename", required=True,
+      					help=("Path to FASTA file containing insert ends to align reads to. "
+							  "Will only retain reads that align well to this DB."))
+	parser.add_argument("-b", "--backbone_db_filename", required=True,
+      					help=("Path to FASTA file containing backbone sequence. "
+							  "Will bin reads by where they align to this sequence."))
+	parser.add_argument("-r", "--read_filenames", nargs='+', required=True,
+						help="Path to FASTQ files containing reads.")
+	parser.add_argument("-t", "--tmp_dir",
+						default="_read_filter_data",
+					    help="Path to use to store intermediate files.")
+	parser.add_argument("--blat_tile_size", type=int, default=10,
+						help="Tile size to use for BLAT search.")
+	parser.add_argument("--blat_step_size", type=int, default=3,
+						help="Step size to use for BLAT search.")
+	parser.add_argument("--blat_min_score", type=int, default=10,
+						help="Minimum score to retain a BLAT match.")
+	args = parser.parse_args()
+
+	# Check that everything we need exists.
+	command_util.CheckAllInstalled(['fastq_to_fasta', 'blat'])
+
+	# Get the filenames we are supposed to process.
+	read_filenames = filename_util.ForceExpand(args.read_filenames)
+	print read_filenames
+	read_filenames = filter(lambda n: n.endswith('fq') or n.endswith('fastq'),
+							read_filenames)
+	print 'Read filenames:', ','.join(read_filenames)
+	assert len(read_filenames) > 0, 'Must provide reads!'
+	# Check that all the input files exist.
+	filename_util.CheckAllExist(read_filenames)
+
+	# Make the temporary directory if needed.
+	if not path.exists(args.tmp_dir):
+		os.makedirs(args.tmp_dir)
+
+	# Convert the FASTQ input files to FASTA as BLAT doesn't seem
+	# to take FASTQ input.
+	print 'Converting FASTQ to FASTA'
+	start_ts = time.time()
+	fasta_fnames = ConvertFASTQToFASTA(read_filenames, args.tmp_dir)
+	duration = time.time() - start_ts
+	print 'Finished converting to FASTA, took %.3f seconds' % duration
+
+	# Align the reads in FASTA format to the insert.
+	print 'Aligning reads to insert database at %s' % args.insert_db_filename
+	start_align_ts = time.time()
+	insert_psl_fnames = AlignReadsToDB(
+		args.insert_db_filename, fasta_fnames, args.tmp_dir,
+		output_filename_postfix='insert_aligned',
+		blat_tile_size=args.blat_tile_size,
+		blat_step_size=args.blat_step_size,
+		blat_min_score=args.blat_min_score)
+	align_duration = time.time() - start_align_ts
+	print 'Finished BLAT alignment to database, took %.3f seconds' % align_duration
+
+	# Produce new FASTA files containing only those reads aligning with insert.
+	start_filter_ts = time.time()
+	filtered_fnames = FilterFASTAByPSL(
+		fasta_fnames, insert_psl_fnames, args.tmp_dir,
+		output_filename_postfix='insert_filtered')
+	filter_duration = time.time() - start_filter_ts
+	print 'Finished filtering output, took %.3f seconds' % filter_duration
+
+	# Align the reads to the backbone.
+	print 'Aligning reads to backbone database at %s' % args.backbone_db_filename
+	start_align_ts = time.time()
+	backbone_psl_fnames = AlignReadsToDB(
+		args.backbone_db_filename, filtered_fnames, args.tmp_dir,
+		output_filename_postfix='backbone_aligned',
+		blat_tile_size=args.blat_tile_size,
+		blat_step_size=args.blat_step_size,
+		blat_min_score=args.blat_min_score)
+	align_duration = time.time() - start_align_ts
+	print 'Finished BLAT alignment to backbone, took %.3f seconds' % align_duration
 
 	total_duration = time.time() - start_ts
 	print 'Done, full script took %.2f minutes' % (total_duration / 60.0)
