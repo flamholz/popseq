35178f856cea0fff8feb752b89a61301fb424fd6
Avi Flamholz <flamholz@gmail.com>
2015-01-30 11:25:10 -0800

    Little fixes to make data aggregation work with command line args.

--- scripts/collect_insert_and_backbone_alignments.py
+++ scripts/collect_insert_and_backbone_alignments.py
@@ -2,28 +2,29 @@
 # <nbformat>2</nbformat>
 
 import argparse
-import glob
 import numpy as np
 
 from sequtils.read_alignment_data import ReadAlignmentData
 
 
 parser = argparse.ArgumentParser(description='Filter reads.')
-parser.add_argument("-i", "--insert_alignments", required=True,
+parser.add_argument("-i", "--insert_alignments", nargs='+', required=True,
                     help=("Path to pslx files containing alignments to backbone"))
-parser.add_argument("-b", "--backbone_alignments", required=True,
+parser.add_argument("-b", "--backbone_alignments", nargs='+', required=True,
                     help=("Path to pslx files containing alignments to insert"))
 parser.add_argument("-r", "--read_filenames", nargs='+', required=True,
                     help="Path to FASTA files containing reads.")
-parser.add_argument("-o", "--output_csv_filename", nargs='+', required=True,
+parser.add_argument("-o", "--output_csv_filename", required=True,
                     help="Where to write CSV output to.")
 args = parser.parse_args()
 
-insert_aligned_fnames = glob.glob(args.insert_alignments)
-backbone_aligned_fnames = glob.glob(args.backbone_alignments)
-fasta_fnames = glob.glob(args.read_filenames)
+insert_aligned_fnames = args.insert_alignments
+backbone_aligned_fnames = args.backbone_alignments
+fasta_fnames = args.read_filenames
 
 """
+# For testing the script with less data
+import glob
 insert_aligned_fnames = glob.glob('sequtils/test/data/DFS001_2_index6_GCCAAT_L004_R1_001_clipped100k_insert_aligned.pslx')
 backbone_aligned_fnames = glob.glob('sequtils/test/data/DFS001_2_index6_GCCAAT_L004_R1_001_clipped100k_backbone_aligned.pslx')
 fasta_fnames = glob.glob('sequtils/test/data/DFS001_2_index6_GCCAAT_L004_R1_001_clipped100k.fa')
@@ -45,6 +46,7 @@ print backbone_aligned_fnames
 print 'FASTA fnames'
 print fasta_fnames
 
+# Gather all the reads information by read ID.
 read_data_by_id = {}
 for insert_fname, backbone_fname, fasta_fname in zip(insert_aligned_fnames,
                                                      backbone_aligned_fnames,
@@ -66,6 +68,17 @@ n_consistent = np.sum(consistent)
 print 'Total reads with any matches:', n_total_w_matches
 print 'Reads with consistent matches to backbone and insert', n_consistent
 
+import csv
+
+print 'Writing insertion matches to', args.output_csv_filename
+with open(args.output_csv_filename, 'w') as f:
+    w = csv.DictWriter(f, ReadAlignmentData.DICT_FIELDNAMES)
+    w.writeheader()
+    for rd in read_data_by_id.itervalues():
+        if rd.has_insertion:
+            w.writerow(rd.AsDict())
+
+"""
 positions = []
 positions_3p = []
 positions_5p = []
@@ -102,17 +115,6 @@ for r in read_data_by_id.itervalues():
         else:
             positions_5p.append(ip)
 
-import csv
-
-print 'Writing insertion matches to', args.output_csv_filename
-with open(args.output_csv_filename, 'w') as f:
-    w = csv.DictWriter(f, ReadAlignmentData.DICT_FIELDNAMES)
-    w.writeheader()
-    for rd in read_data_by_id.itervalues():
-        if rd.has_insertion:
-            w.writerow(rd.AsDict())
-
-"""
 import pylab
 pylab.figure()
 pylab.hist(positions_3p, bins=50, color='b', label='3p insertions')
