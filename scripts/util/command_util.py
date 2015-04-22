3ca652183270da4ca3b1d08fb059d5527771844c
Avi Flamholz <flamholz@gmail.com>
2015-04-22 10:21:57 -0700

    Add more BLAT parameters for use.

--- scripts/util/command_util.py
+++ scripts/util/command_util.py
@@ -13,16 +13,19 @@ def ToFASTACommand(fastq_path, fasta_path, keep_degenerate=True):
 		l.append('-n')
 	return l
 
+
 def BLATCommand(db_fname, query_fname, output_fname,
-				blat_tile_size=10, blat_step_size=3,
-				blat_min_score=10, blat_max_gap=0,
-				output_type='psl'):
+				tile_size, step_size, min_score, max_gap,
+				one_off, rep_match, output_type='pslx'):
 	"""Returns a shell to BLAT search with."""
 	command = ['blat', db_fname, query_fname, output_fname,
 			   '-q=dna', '-t=dna',  # DB and query are DNA
-			   '-tileSize=%d' % blat_tile_size,
-			   '-stepSize=%d' % blat_step_size,
-			   '-minScore=%d' % blat_min_score,
+			   '-tileSize=%d' % tile_size,
+			   '-stepSize=%d' % step_size,
+			   '-minScore=%d' % min_score,
+			   '-maxGap=%d' % max_gap,
+			   '-oneOff=%d' % one_off,
+			   '-repMatch=%d' % rep_match,
                '-out=%s' % output_type] 
 	return command
 
