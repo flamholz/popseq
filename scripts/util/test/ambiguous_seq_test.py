5b8ee12b612564e3cc56a1d82280a479a6f0342a
Avi Flamholz <flamholz@gmail.com>
2015-05-06 18:49:19 -0700

    Add some docstrings.

--- scripts/util/test/ambiguous_seq_test.py
+++ scripts/util/test/ambiguous_seq_test.py
@@ -11,6 +11,8 @@ class AmbiguousSeqTest(unittest.TestCase):
     def assertAmbiguousSeqInstance(self, ambiguous, defined, n):
         i = 0
         ambiguous = ambiguous.upper()
+        # n is the number of repeats
+        self.assertEquals(len(ambiguous) * n, len(defined))
         for _ in xrange(n):
             for ba in ambiguous:
                 allowed_bases = IUPACData.ambiguous_dna_values[ba]
@@ -27,6 +29,8 @@ class AmbiguousSeqTest(unittest.TestCase):
             gen = AmbiguousSequenceGenerator(ambig)
             for n in xrange(10):
                 seq = gen.Generate(n=n)
+                # Check that the generated sequence could have been generated 
+                # from the ambiguous sequence.
                 self.assertAmbiguousSeqInstance(ambig, seq, n)
 
 
