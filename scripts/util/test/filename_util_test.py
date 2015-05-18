95e247fee63d9392c91604455b1b4538e3660607
Avi Flamholz <flamholz@gmail.com>
2015-05-17 19:57:47 -0700

    Remove cruft and fix broken tests from rename.

--- scripts/util/test/filename_util_test.py
+++ scripts/util/test/filename_util_test.py
@@ -14,7 +14,7 @@ class FilenameUtilTests(unittest.TestCase):
 					  ('asdkmals_askdm12.bz2', 'asdkmals_askdm12.fq'),
 					  ('/home/ban/maggots/s123_sd.mmq', 's123_sd.fq'))
 		for input_fname, expected_out in test_cases:
-			out_fname = filename_util._MakeFname(input_fname, out_ext)
+			out_fname = filename_util.MakeFname(input_fname, out_ext)
 			self.assertEquals(expected_out, out_fname)
 
 	def testMakeFnameDestDir(self):
@@ -26,7 +26,7 @@ class FilenameUtilTests(unittest.TestCase):
 					  ('/home/ban/maggots/s123_sd.mmq',
 					   '%s/s123_sd.fq' % dest_dir))
 		for input_fname, expected_out in test_cases:
-			out_fname = filename_util._MakeFname(
+			out_fname = filename_util.MakeFname(
 				input_fname, out_ext, dest_dir=dest_dir)
 			self.assertEquals(expected_out, out_fname)
 
@@ -39,7 +39,7 @@ class FilenameUtilTests(unittest.TestCase):
 					  ('/home/ban/maggots/s123_sd.mmq',
 					   's123_sd_%s.fq' % postfix))
 		for input_fname, expected_out in test_cases:
-			out_fname = filename_util._MakeFname(
+			out_fname = filename_util.MakeFname(
 				input_fname, out_ext, postfix=postfix)
 			self.assertEquals(expected_out, out_fname)
 
@@ -53,7 +53,7 @@ class FilenameUtilTests(unittest.TestCase):
 					  ('/home/ban/maggots/s123_sd.mmq',
 					   '%s/s123_sd_%s.fq' % (dest_dir, postfix)))
 		for input_fname, expected_out in test_cases:
-			out_fname = filename_util._MakeFname(
+			out_fname = filename_util.MakeFname(
 				input_fname, out_ext, dest_dir=dest_dir,
 				postfix=postfix)
 			self.assertEquals(expected_out, out_fname)
