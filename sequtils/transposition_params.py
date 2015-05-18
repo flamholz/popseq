17d0e589e89bda0ce9f69bac3420b9ef28f79e58
Avi Flamholz <flamholz@gmail.com>
2015-05-17 19:16:42 -0700

    TranspositionParams now knows about possible linker sequences.

--- sequtils/transposition_params.py
+++ sequtils/transposition_params.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 
 from Bio import SeqIO
+from Bio.Seq import Seq
+from sequtils.ambiguous_seq import AmbiguousSequence
 
 
 class TranspositionParams(object):
     """Parameters associated with a transposition library."""
     
     def __init__(self, insert_seq, backbone_seq, backbone_start_offset,
-                 fixed_seq_5p, fixed_seq_3p, tn_bp_duplicated):
+                 fixed_seq_5p, fixed_seq_3p, linker_pattern,
+                 max_linker_repeats, tn_bp_duplicated):
         """Initialize.
         
         Args:
@@ -21,27 +24,70 @@ class TranspositionParams(object):
                 the insert.
             fixed_seq_3p: the fixed sequence found on the 3' end of
                 the insert.
+            linker_pattern: AmbiguousSequence object defining the pattern.
+            max_linker_repeats: the maximum number of repeats of the
+                linker pattern.
             tn_bp_duplicated: the number of bases duplicated during
                 transposition.
         """
+        assert insert_seq
+        assert backbone_seq
+        assert fixed_seq_5p
+        assert fixed_seq_3p
+        
         self.insert_seq = insert_seq
         self.backbone_seq = backbone_seq
         self.backbone_start_offset = backbone_start_offset
         self.fixed_seq_5p = fixed_seq_5p
         self.fixed_seq_3p = fixed_seq_3p
+        self.linker_pattern = linker_pattern
+        self.max_linker_repeats = max_linker_repeats
         self.tn_bp_duplicated = tn_bp_duplicated
         
-        self.fixed_seq_by_end = {'3p': self.fixed_seq_3p,
-                                 '5p': self.fixed_seq_5p}
+        self.fixed_seq_by_end = {('3p', 1): self.fixed_seq_3p,
+                                 ('5p', 1): self.fixed_seq_5p,
+                                 ('3p', -1): self.fixed_seq_3p.reverse_complement(),
+                                 ('5p', -1): self.fixed_seq_5p.reverse_complement()}
+    
+    def GetFixedSequence(self, fixed_end, orientation):
+        return self.fixed_seq_by_end[(fixed_end, orientation)]
+    
+    def GetInsertEndSequence(self, fixed_end, orientation, n):
+        seq = self.insert_seq[:n]
+        if fixed_end == '3p':
+            seq = self.insert_seq[-n:]
+        if orientation == -1:
+            seq = seq.reverse_complement()
+        return seq
+    
+    def ValidLinker(self, linker_seq):
+        """Returns True if this linker sequence is valid.
         
-    @staticmethod
-    def FromArgs(args):
+        i.e. does the linker match the known pattern and fall within the
+        maximum number of repeats.
+        
+        Args:
+            linker_seq: sequence observed for the linker.
+        """
+        n = self.linker_pattern.IsInstance(linker_seq)
+        valid = n >= 0 and n <= self.max_linker_repeats
+        return valid
+            
+    @classmethod
+    def LoadFASTA(cls, fname):
+        return SeqIO.parse(fname, 'fasta').next().seq
+    
+    @classmethod
+    def FromArgs(cls, args):
         """Parse from commandline arguments in a standard form."""
-        insert_seq = SeqIO.parse(args.insert_seq_filename, 'fasta').next()
-        bbone_seq = SeqIO.parse(args.backbone_db_filename, 'fasta').next()
+        insert_seq = cls.LoadFASTA(args.insert_seq_filename)
+        bbone_seq = cls.LoadFASTA(args.backbone_db_filename)
+        linker_pattern = AmbiguousSequence(args.linker_pattern)
         return TranspositionParams(insert_seq, bbone_seq, args.start_offset,
-                                   args.fixed_5p, args.fixed_3p, args.tn_bp_duplicated)
-        
+                                   Seq(args.fixed_5p), Seq(args.fixed_3p),
+                                   linker_pattern, args.max_linker_repeats,
+                                   args.tn_bp_duplicated)
+    
     def __str__(self):
         l = ['<TranspositionParams backbone_start_offset=%d' % self.backbone_start_offset,
              'fixed_seq_5p="%s"' % self.fixed_seq_5p,
